0c43ac0fdd531f7ca1d8df97d384177273713173
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-09-20 18:00:09 -0700

    update driver code in main.rb

--- main.rb
+++ main.rb
@@ -1,7 +1,16 @@
 require_relative 'planet'
+require_relative 'solar_system'
 
 def main
+  solar_system = SolarSystem.new('Sol')
 
+  earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
+  solar_system.add_planet(earth)
+
+  list = solar_system.list_planets
+  puts list
+  # => Planets orbiting Sol
+  # => 1.  Earth
 end
 
 main
