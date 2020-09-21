e4fadf452974801c7739e1897b545b7ce3685e34
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-09-20 17:53:04 -0700

    add add_planet method

--- solar_system.rb
+++ solar_system.rb
@@ -1,9 +1,14 @@
 class SolarSystem
-  
+
   attr_reader :star_name, :planets
 
   def initialize(star_name)
     @star_name = star_name
     @planets = []
   end
+
+  def add_planet(new_planet)
+    @planets << new_planet
+  end
+
 end
