3dcf27dae09fab855f08e8fe4870c3a96dc6bc4c
Tram Bui <pbui17@cmc.edu>
2020-09-21 16:48:35 -0500

    completed wave 2

--- main.rb
+++ main.rb
@@ -1,11 +1,24 @@
 require_relative 'planet'
+require_relative 'solar_system'
 
-def main
-  earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
-  mars = Planet.new("Mars", "red", 6.39e23, 1.307e8, "where The Martian took place")
-  puts earth.summary
-  puts mars.summary
+# def main
+#   earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
+#   mars = Planet.new("Mars", "red", 6.39e23, 1.307e8, "where The Martian took place")
+#   puts earth.summary
+#   puts mars.summary
+#
+# end
+#
+# main
 
-end
+solar_system = SolarSystem.new("Sol")
+earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
+solar_system.add_planet(earth)
 
-main
+list = solar_system.list_planets
+puts list
+
+found_planet = solar_system.find_planet_by_name('Earth')
+puts found_planet
+
+puts found_planet.summary
