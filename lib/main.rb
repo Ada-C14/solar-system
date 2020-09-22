ba3b4ddf356f5575c916414aacdaf266e694fb57
Lina Do <l.do5147@gmail.com>
2020-09-21 21:14:17 -0700

    moved code around in main method to look cleaner

--- lib/main.rb
+++ lib/main.rb
@@ -6,9 +6,9 @@ def main
   solar_system = SolarSystem.new('Sol')
 
   earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
-  solar_system.add_planet(earth)
-
   mars = Planet.new('Mars', 'red', 6.39e23, 2.105e8, 'The planet named after the Roman God of war.' )
+  
+  solar_system.add_planet(earth)
   solar_system.add_planet(mars)
 
   list = solar_system.list_planets
