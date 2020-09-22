4f1db338c6239ed71c4a957bb58267b6af6a83bd
Christabel Sebastian <christabel.escarez@gmail.com>
2020-09-21 20:35:37 -0700

    Unsure of syntax writing tests for Main; no tests implemented in main_test.rb

--- test/main_test.rb
+++ test/main_test.rb
@@ -9,7 +9,14 @@ require_relative '../lib/solar_system'
 # Get that nice colorized output
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-describe "Solar Systems program" do
-
+#Unsure of syntax for Main -- the below have not been implemented
+xdescribe "Solar Systems program" do
+  it "check for ArgumentError if user enters non numerical selection or a number not available to select" do
+    # Arrange && Act && Assert
+    expect {
+      main
+      "A"
+    }.must_raise ArgumentError
+  end
 
 end
