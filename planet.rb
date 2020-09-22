2072b4e27ebea44f468bd0ac3f43e8417af73df1
Stacy <lundquist.stacy@gmail.com>
2020-09-22 15:43:41 -0700

    Wave2Step1

--- planet.rb
+++ planet.rb
@@ -10,8 +10,8 @@ class Planet
     @inhabited = inhabited
   end
 
-  # raise ArgumentError "uhhh shouldn't it be greater than zero" if :mass_kg < 0
-  # raise ArgumentError "Are we on the sun?" if :distance_from_sun_km < 0
+  # raise ArgumentError "uhhh shouldn't it be greater than zero" if mass_kg < 0
+  # raise ArgumentError "Are we on the sun?" if distance_from_sun_km < 0
 
   def summary
     return "#{name} is a #{color} planet #{distance_from_sun_km} kilometers away from the sun. Rumor has it its inhabitants are called #{inhabited}, and #{fun_fact}! It is a healthy #{mass_kg} kilograms."
---
+++ solar_system.rb
@@ -0,0 +1,13 @@
+class SolarSystem
+  attr_reader :star_name, :planets
+
+  def initialize(star_name)
+    @star_name = star_name
+    @planets = []
+  end
+
+
+
+
+
+end
