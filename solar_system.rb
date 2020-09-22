c91d169efdb5addaa0b7ed5d0430b98ebb582ff5
Li Dai <li.lea.dai@gmail.com>
2020-09-22 15:04:31 -0700

    fixed indentation and lines formatting

--- solar_system.rb
+++ solar_system.rb
@@ -29,16 +29,19 @@ class SolarSystem
 
   def distance_between(planet_a, planet_b)
     distance_arry = []
+
     @planets.each do |planet|
       if planet.name.downcase == planet_a.downcase || planet.name.downcase == planet_b.downcase
         distance_arry << planet.distance_from_sun_km
       end
     end
+
     begin
       return "The distance between #{planet_a} and #{planet_b} is #{(distance_arry[0] - distance_arry[1]).abs} km."
     rescue TypeError, NoMethodError
       print "One of the planets is not found in our system. Returning to main menu..."
     end
+
   end
 
 end
