aec882ce76b420814c30db7ada23e7603803014c
Stacy <lundquist.stacy@gmail.com>
2020-09-22 15:20:11 -0700

    I tried the optional, didn't work

--- main.rb
+++ main.rb
@@ -1,6 +1,12 @@
 require_relative 'planet'
 
 def main
+  # Load Planet into pry:
+  # $ pry -r ./planet.rb
+  earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life', 'Humans')
+
+  puts earth.summary
+
 
 end
 
--- planet.rb
+++ planet.rb
@@ -10,8 +10,11 @@ class Planet
     @inhabited = inhabited
   end
 
+  # raise ArgumentError "uhhh shouldn't it be greater than zero" if :mass_kg < 0
+  # raise ArgumentError "Are we on the sun?" if :distance_from_sun_km < 0
+
   def summary
-    return "#{name} is a #{color} planet #{distance_from_sun_km} away from the sun. Rumor has it its inhabitants are called #{inhabited}, and #{fun_fact}! It is a healthy #{mass_kg} kilograms."
+    return "#{name} is a #{color} planet #{distance_from_sun_km} kilometers away from the sun. Rumor has it its inhabitants are called #{inhabited}, and #{fun_fact}! It is a healthy #{mass_kg} kilograms."
   end
 end
 
