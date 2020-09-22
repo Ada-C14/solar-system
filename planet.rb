6e41ba64818fd6509f138c94c36633f27e0caf02
Stacy <lundquist.stacy@gmail.com>
2020-09-22 14:57:01 -0700

    Wave1, Step 2

--- planet.rb
+++ planet.rb
@@ -1,3 +1,26 @@
 class Planet
+  attr_reader :name, :color, :mass_kg, :distance_from_sun_km, :fun_fact, :inhabited
+
+  def initialize(name, color, mass_kg, distance_from_sun_km, fun_fact, inhabited)
+    @name = name
+    @color = color
+    @mass_kg = mass_kg
+    @distance_from_sun_km = distance_from_sun_km
+    @fun_fact = fun_fact
+    @inhabited = inhabited
+  end
 
 end
+
+# Load Planet into pry:
+# $ pry -r ./planet.rb
+earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life', 'yes')
+
+puts earth.name
+# => Earth
+puts earth.fun_fact
+# => Only planet known to support life
+
+earth.color = 'pink'
+# => NoMethodError: undefined method `color=' for #<Planet:0x00007fcfba04c130>
+# => Did you mean?  color
