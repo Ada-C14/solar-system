d7101244feb3df6abeec750f118dc69ee4f89c25
Kayla Johnson <kayla.elizabeth89@gmail.com>
2020-09-22 15:32:26 -0700

    created new instance of earth and tested code from planet class

--- main.rb
+++ main.rb
@@ -2,11 +2,12 @@ require_relative 'planet'
 require 'colorize'
 require 'colorized_string'
 
+
 def main
 
 end
 
-pp earth.summary
+earth = Planet.new("earth", "blue-green",-1,2,"I live on it")
 
-
+puts earth.summary
 main
