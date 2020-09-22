708434eeae8e4cca6c6d1a99266d3aa98e26e811
Genevieve Neely <genevieve.hood@gmail.com>
2020-09-22 08:56:35 -0700

    symbols

--- planet.rb
+++ planet.rb
@@ -1,6 +1,6 @@
 
 class Planet
-  attr_reader(name, color, mass_kg, distance_from_sun_km, fun_fact)
+  attr_reader(:name, :color, :mass_kg, :distance_from_sun_km, :fun_fact)
 
   def initialize(name, color, mass_kg, distance_from_sun_km, fun_fact)
     @name = name
