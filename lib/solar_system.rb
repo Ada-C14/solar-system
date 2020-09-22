c827861e99283a2409a97017543e30220d49f0fc
Christabel Sebastian <christabel.escarez@gmail.com>
2020-09-21 18:17:32 -0700

    Titleize SolarSytem.star_name

--- lib/solar_system.rb
+++ lib/solar_system.rb
@@ -8,7 +8,7 @@ class SolarSystem
   attr_reader :star_name, :planets
 
   def initialize(star_name)
-    @star_name = star_name
+    @star_name = star_name.split(' ').map(&:capitalize).join(' ')
     @planets = []
   end
 
