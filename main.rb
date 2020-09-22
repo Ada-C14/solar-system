a9ee4f3012f38dd707f2e725df739594be4c7b48
Kayla Johnson <kayla.elizabeth89@gmail.com>
2020-09-21 23:33:06 -0700

    updated planet and created main.rb file

---
+++ main.rb
@@ -0,0 +1,9 @@
+require_relative 'planet'
+
+def main
+
+end
+
+
+
+main
--- planet.rb
+++ planet.rb
@@ -10,3 +10,9 @@ class Planet
     @fun_fact = fun_fact
   end
 end
+
+earth = Planet.new('Earth', "blue-green", 5.972e24, 1.496e8, "Only planet known to support life")
+
+puts earth.name
+puts earth.fun_fact
+# earth.color = "pink"
