7af72c37b5650f31f2e5e8556a69f41f87efd102
Rachael Gomez <gomezrc1220@gmail.com>
2020-09-22 16:43:00 -0700

    Created planet class

---
+++ main.rb
@@ -0,0 +1,6 @@
+require_relative 'planet'
+
+earth = Planet.new("Earth",'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
+
+
+puts earth.summary('Earth','blue-green',5.972e24, 1.496e8, 'Only planet known to support life' )
