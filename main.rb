6149423479c0b7ed08af90a31b8cda817c62bab9
Ringo Alcock <ringo.alcock@gmail.com>
2020-09-22 16:18:29 -0700

    build CLI in main.rb to replace driver code

--- main.rb
+++ main.rb
@@ -4,41 +4,80 @@ require_relative "solar_system"
 def main
   solar_system = SolarSystem.new("Sol")
 
-  trill = Planet.new("Trill", "Green", 1000, 1000, "Jadzia's home")
+  trill = Planet.new("Trill", "Purple", 1.372e24, 1.206e5, "It is full of caves!")
   solar_system.add_planet(trill)
 
-  cardassia = Planet.new("Cardassia", "Brown", 2000, 2000, "Garak's home")
+  cardassia = Planet.new("Cardassia", "Brown", 5.012e16, 2.365e7, "It is very hot!")
   solar_system.add_planet(cardassia)
 
-  bajor = Planet.new("Bajor", "Red", 3000, 3000, "Kira's home")
+  earth = Planet.new("Qo'noS", "Green", 5.972e24, 1.496e8, "It is hard to spell!")
+  solar_system.add_planet(earth)
+
+  bajor = Planet.new("Bajor", "Blue", 3.721e4, 1.916e12, "It is by a wormhole!")
   solar_system.add_planet(bajor)
 
-  list = solar_system.list_planets
-  puts list
-  puts solar_system.distance_between("Cardassia", "Bajor")
-  #
-  # puts "what is the star in your solar system?"
-  # star_name = gets.chomp
-  # solar_system = SolarSystem.new(star_name)
-  # 2.times do
-  #   puts "what planet would you like to add?"
-  #   name = gets.chomp
-  #   puts "what color is the planet?"
-  #   color = gets.chomp
-  #   puts "what is the mass of the planet in kg?"
-  #   mass_kg = gets.chomp.to_f
-  #   puts "how far is the planet from its sun in km?"
-  #   distance_from_sun_km = gets.chomp.to_f
-  #   puts "what's something neat about the planet?"
-  #   fun_fact = gets.chomp
-  #   planet = Planet.new(name, color, mass_kg, distance_from_sun_km, fun_fact)
-  #   puts "#{planet.name} is #{planet.color}"
-  #   solar_system.add_planet(planet)
-  # end
-  # puts solar_system.list_planets
-  # return solar_system
+  selection = ""
+  while selection != "exit"
+    puts "What would you like to do?\nlist planets\nplanet details\nadd planet\nfind distance\nexit"
+    selection = gets.chomp.downcase
+    case selection
+    when "list planets"
+      puts solar_system.list_planets
+    when "planet details"
+      # add a helper method to check that planet is actually in solar_system?
+      puts "What planet would you like to know about?\n#{solar_system.list_planets}"
+      curious_planet = gets.chomp
+      curious_planet_answers = solar_system.find_planet_by_name(curious_planet)
+      puts curious_planet_answers.summary
+    when "add planet"
+      puts "What is the name of this new planet?"
+      new_name = gets.chomp
+      puts "What color is the new planet?"
+      new_color = gets.chomp
+      puts "What is the mass of the new planet in kg?"
+      new_mass = gets.chomp.to_f
+      puts "How far is the new planet from #{solar_system.star_name} in km?"
+      new_distance = gets.chomp.to_f
+      puts "What is a fun fact about the new planet?"
+      new_fact = gets.chomp
+      new_planet = Planet.new(new_name, new_color, new_mass, new_distance, new_fact)
+      solar_system.add_planet(new_planet)
+    when "find distance"
+      # add a helper method to check that planet is actually in solar_system?
+      puts "what is the planet you are starting from?"
+      planet1 = gets.chomp
+      puts "what is the planet you are ending on?"
+      planet2 = gets.chomp
+      puts solar_system.distance_between(planet1, planet2)
+    else
+      puts "that is not a valid selection"
+      # TODO Ringo make this make sense
+      # if I do not have a
+      # selection = gets.chomp
+      # line, than putting "exit" puts "that is not a valid selection" before ending
+      # if I do have it then it bugs user to enter their selection AGAIn
+      # at the top of the loop
+    end
+  end
 end
 
-main
+# TODO Ringo get rid of this
+# def scratchpad
+#   solar_system = SolarSystem.new("Sol")
+#
+#   trill = Planet.new("Trill", "Purple", 1.372e24, 1.206e5, "It is full of caves!")
+#   solar_system.add_planet(trill)
+#
+#   cardassia = Planet.new("Cardassia", "Brown", 5.012e16, 2.365e7, "It is very hot!")
+#   solar_system.add_planet(cardassia)
+#
+#   earth = Planet.new("Qo'noS", "Green", 5.972e24, 1.496e8, "It is hard to spell!")
+#   solar_system.add_planet(earth)
+#
+#   bajor = Planet.new("Bajor", "Blue", 3.721e4, 1.916e12, "It is by a wormhole!")
+#   solar_system.add_planet(bajor)
+#
+# end
 
+main
 
