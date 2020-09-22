84135038cec4da9f8aea272ac650f0b30c28e2d1
Genevieve Neely <genevieve.hood@gmail.com>
2020-09-22 08:54:36 -0700

    create Planet class

---
+++ planet.rb
@@ -0,0 +1,17 @@
+
+class Planet
+  attr_reader(name, color, mass_kg, distance_from_sun_km, fun_fact)
+
+  def initialize(name, color, mass_kg, distance_from_sun_km, fun_fact)
+    @name = name
+    @color = color
+    @mass_kg = mass_kg
+    @distance_from_sun_kg = distance_from_sun_km
+    @fun_fact = fun_fact
+  end
+
+  def summary
+    return "Summary for #{@planet.capitalize}:\nColor: #{@color}\nMass: #{@mass_kg}kg\nDistance from Sun:
+            #{@distance_from_sun_km}km\nFun fact: #{@fun_fact}"
+  end
+end
