30ac807de0e8f80ce72aee389fff59ba1536489d
Lina Do <l.do5147@gmail.com>
2020-09-21 18:08:57 -0700

    included a summary method in the Planet class

---
+++ lib/main.rb
@@ -0,0 +1,7 @@
+require_relative 'planet'
+
+def main
+  # create two different instances of planet and print out their attributes
+end
+
+main
--- lib/planet.rb
+++ lib/planet.rb
@@ -8,4 +8,8 @@ class Planet
     @distance_from_sun_km = distance_from_sun_km
     @fun_fact = fun_fact
   end
+
+  def summary
+    return "Planet Name: #{@name}\n Color: #{@color}\n Mass: #{@mass_kg}kg\n Distance from the Sun:#{@distance_from_sun_km}kg \nFun fact: #{@fun_fact}."
+  end
 end
