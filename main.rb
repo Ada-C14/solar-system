5de313cfd7fd5280c351b61c8662806723f7dd78
Kareha Agesa <kmaagesa@gmail.com>
2020-09-22 14:29:33 -0700

    adding add_planet option to control loop

--- main.rb
+++ main.rb
@@ -12,13 +12,13 @@ def main
   solar_system.add_planet(earth)
   solar_system.add_planet(jupiter)
 
-  options = ["1. list planets", "2. planet details", "3. exit"]
+  options = ["1. list planets", "2. planet details", "3. add planet", "4. exit"]
   answer = ""
   until answer == "exit"
-    puts "What do you want to do? Options: \n#{options.join("\n")}"
+    puts "What do you want to do next? Options: \n#{options.join("\n")}"
     answer = gets.chomp.downcase
     until options.map{|i| i.split(".")[-1].strip}.include?(answer)
-      puts "sorry this is not a valid answer. Please choose either: \n#{options.join("\n")}"
+      puts "Sorry this is not a valid answer. Please choose either: \n#{options.join("\n")}"
       answer = gets.chomp.downcase
     end
     if answer == "list planets"
@@ -28,6 +28,20 @@ def main
       planet = gets.chomp
       found_planet = solar_system.find_planet_by_name(planet)
       puts found_planet.summary
+    elsif answer == "add planet"
+      puts "Please enter the details of the planet you want to add, starting with its name"
+      name = gets.chomp.capitalize
+      puts "Now enter its color"
+      color = gets.chomp
+      puts "Now enter its mass in kg"
+      mass_kg = gets.chomp.to_i
+      puts "Now enter its distance from the sun in km"
+      distance_from_sun_km = gets.chomp.to_i
+      puts "Now enter a fun fact about it"
+      fun_fact = gets.chomp
+      user_planet = Planet.new(name, color, mass_kg, distance_from_sun_km, fun_fact)
+      solar_system.add_planet(user_planet)
+      puts "Sweet, here's the new planet list: \n#{solar_system.list_planets}"
     end
   end
   # puts solar_system.list_planets
