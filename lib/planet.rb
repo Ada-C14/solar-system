ecd6f813744fb13d9b4e89543446c2e96b84b943
Christabel Sebastian <christabel.escarez@gmail.com>
2020-09-21 19:38:49 -0700

    Added test for empty planet name, and default values for nil/empty color, nil numbers string numbers, and nil fun fact. Also added color string->symbol.

--- lib/planet.rb
+++ lib/planet.rb
@@ -7,21 +7,34 @@ class Planet
   attr_reader :name, :color, :mass_kg, :distance_from_sun_km, :fun_fact
 
   def initialize(hash)
+    raise ArgumentError, "Need planet name." if (!hash[:name].to_s.strip || hash[:name].to_s.strip.empty?)
+
     @name = hash[:name].split(' ').map(&:capitalize).join(' ')
-    @color = hash[:color]
-    @mass_kg = natural_num?(hash[:mass_kg])
-    @distance_from_sun_km = natural_num?(hash[:distance_from_sun_km])
-    @fun_fact = hash[:fun_fact]
+    @color = symbol?(hash[:color])
+    @mass_kg = positive_real_num?(hash[:mass_kg])
+    @distance_from_sun_km = positive_real_num?(hash[:distance_from_sun_km])
+    @fun_fact = hash[:fun_fact] ? hash[:fun_fact] : "TBD"
   end
 
   def summary
     return "The planet #{@name} is a lovely #{@color.to_s} color and weighs #{@mass_kg} kilograms. #{@name} is #{@distance_from_sun_km} kilometers from the sun and fun fact: #{@fun_fact}"
   end
 
-  def natural_num?(num)
+  def positive_real_num?(num)
+    raise  ArgumentError, "Each planet must have a mass (kg) and distance from sun (km)." if !num
     raise ArgumentError, "Cannot have a negative mass or negative distance." if num.to_f < 0
     raise ArgumentError, "Please enter a number." if (num.to_f == 0 && !num.match(/[0]+[.]?[0]*/))
     return num.to_f
+  end
+
+  def symbol?(input)
+    if (!input || input == "")
+      return :unknown
+    elsif input.class == Symbol
+      return input.downcase
+    else
+      return input.downcase.to_sym
+    end
   end
 
 end
--- test/planet_test.rb
+++ test/planet_test.rb
@@ -52,21 +52,100 @@ describe "Planets class" do
       expect{ test_planet = Planet.new() }.must_raise ArgumentError, "wrong number of arguments"
     end
 
-        it "check if planets.name is correctly titleized " do
-      #Arrange
-      #nothing to arrange, except planet.rb
+      it "check if planets.name is correctly titleized " do
+        #Arrange
+        #nothing to arrange, except planet.rb
 
-      # Act
-      test_planet = Planet.new(
-        name: "teSt PlaNet",
-        color: :pink,
+        # Act
+        test_planet = Planet.new(
+          name: "teSt PlaNet",
+          color: :pink,
+          mass_kg: 1e10,
+          distance_from_sun_km: 1e10,
+          fun_fact: "Planet constructor titleization test."
+        )
+
+        # Assert
+        expect(test_planet.name).must_equal "Test Planet"
+      end
+
+    it "check for ArgumentError if no planet name is entered" do
+      # Arrange && Act && Assert
+      expect{
+        test_planet = Planet.new(
+            name: "",
+            color: :pink,
+            mass_kg: "fat",
+            distance_from_sun_km: 1e10,
+            fun_fact: "Empty name test."
+        ) }.must_raise ArgumentError, "Need planet name."
+      expect{
+        test_planet = Planet.new(
+            color: :pink,
+            mass_kg: 1e10,
+            distance_from_sun_km: "hecka far",
+            fun_fact: "nil name test."
+        ) }.must_raise ArgumentError, "Need planet name."
+    end
+
+    it "check for :unknown color if none is entered" do
+      # Arrange
+      test_planet1 = Planet.new(
+        name: "Test Planet",
+        color: "",
         mass_kg: 1e10,
         distance_from_sun_km: 1e10,
-        fun_fact: "Planet constructor titleization test."
+        fun_fact: "Empty color test."
+      )
+      test_planet2 = Planet.new(
+        name: "Test Planet",
+        mass_kg: 1e10,
+        distance_from_sun_km: 1e10,
+        fun_fact: "nil color test."
       )
 
+      # Act
+      planet1_color = test_planet1.color
+      planet2_color = test_planet2.color
+
       # Assert
-      expect(test_planet.name).must_equal "Test Planet"
+      expect(planet1_color).must_equal :unknown
+      expect(planet2_color).must_equal :unknown
+    end
+
+    it "check if string color inputs get converted to symbol" do
+      # Arrange
+      test_planet = Planet.new(
+        name: "Test Planet",
+        color: "PINK",
+        mass_kg: 1e10,
+        distance_from_sun_km: 1e10,
+        fun_fact: "String color test."
+      )
+
+      # Act
+      planet_color = test_planet.color
+
+      # Assert
+      expect(planet_color).must_equal :pink
+    end
+
+    it "check for ArgumentError if no mass or distance is entered" do
+      # Arrange && Act && Assert
+      expect{
+        test_planet = Planet.new(
+            name: "Test Planet",
+            color: :pink,
+            distance_from_sun_km: 1e10,
+            fun_fact: "nil mass test."
+        ) }.must_raise ArgumentError, "Each planet must have a mass (kg) and distance from sun (km)."
+      expect{
+        test_planet = Planet.new(
+            name: "Test Planet",
+            color: :pink,
+            mass_kg: 1e10,
+            fun_fact: "nil distance test."
+        ) }.must_raise ArgumentError, "Each planet must have a mass (kg) and distance from sun (km)."
     end
 
     it "ArgumentError when mass or distance < 0" do
@@ -107,6 +186,20 @@ describe "Planets class" do
             distance_from_sun_km: "hecka far",
             fun_fact: "String as distance test."
         ) }.must_raise ArgumentError, "Please enter a number."
+    end
+
+
+    it "check for 'TBD' if no fun fact input" do
+      # Arrange
+      test_planet = Planet.new(
+        name: "Test Planet",
+        color: :pink,
+        mass_kg: 1e10,
+        distance_from_sun_km: 1e10,
+      )
+
+      # Act && Assert
+      expect(test_planet.fun_fact).must_equal "TBD"
     end
   end
 
