bbb9df2f4a9cf730bb99dd03f89a2b98b67fa313
Sophie Messing <sophie.e.messing@gmail.com>
2020-09-22 14:43:24 -0700

    control loop and exit is working

--- main.rb
+++ main.rb
@@ -26,57 +26,40 @@ def main
 
   ###################################
 
-  puts "Welcome to the solar system."
-  puts "Please enter 'list planets' or 'exit' to continue."
-  input = gets.chomp.downcase!
+  while true
+    puts "Welcome to the Solar System!"
+    puts "Press 'exit' to quit at any time"
+    puts "Please enter 'list planets', 'planet details', or 'add planet'"
+    input = gets.chomp
 
-  until input = "list planets" || input = "exit"
-    puts "Invalid input. Please enter 'list planets' or 'exit' to continue."
-    input = gets.chomp.downcase!
+    case input
+      when "list planets"
+        puts solar_system.list_planets
+      when "planet details"
+        puts "Please enter a planet name for more details."
+        planet_name = gets.chomp.capitalize!
+        found_planet = solar_system.find_planet_by_name(planet_name)
+        puts found_planet.summary
+      when "add planet"
+        puts "What is the name of the planet?"
+          name = gets.chomp.capitalize!
+        puts "What color is it?"
+          color = gets.chomp.downcase!
+        puts "What is the planet's mass in kg?"
+          mass_kg = gets.chomp.to_i
+        puts "How far is the planet from the sun in km?"
+          distance_from_sun_km = gets.chomp.to_i
+        puts "What's a fun fact about this planet?"
+          fun_fact = gets.chomp
+        solar_system.add_planet(Planet.new(name, color, mass_kg, distance_from_sun_km, fun_fact))
+      when "exit"
+        break
+      else
+        puts "Invalid input"
+    end
+
+
   end
-
-  if input == "list planets"
-    puts solar_system.list_planets
-  elsif input == "exit"
-    exit!
-  end
-
-  puts "Please enter a planet name for planet details."
-  planet_name = gets.chomp.capitalize!
-  found_planet = solar_system.find_planet_by_name(planet_name)
-  puts found_planet.summary
-
-  puts "Would you like to add a planet to the solar system? Enter 'yes' or 'no'"
-  input = gets.chomp.downcase!
-
-  until input = "yes" || input = "no"
-    puts "Invalid input. Please enter 'yes' or 'no' to continue."
-    input = gets.chomp.downcase!
-  end
-
-  if input == "yes"
-    puts "What is the name of the planet?"
-      name = gets.chomp.capitalize!
-    puts "What color is it?"
-      color = gets.chomp.downcase!
-    puts "What is the planet's mass in kg?"
-      mass_kg = gets.chomp.to_i
-    puts "How far is the planet from the sun in km?"
-      distance_from_sun_km = gets.chomp.to_i
-    puts "What's a fun fact about this planet?"
-      fun_fact = gets.chomp
-
-    new_planet = Planet.new(name, color, mass_kg, distance_from_sun_km, fun_fact)
-    # add_planet(new_planet)
-    solar_system.add_planet(new_planet)
-
-  elsif input == "no"
-    exit
-  end
-
-  puts new_planet.summary
-  puts solar_system.list_planets
-
 end
 
 main
