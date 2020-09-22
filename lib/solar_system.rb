255037c524b3c6d935363a8f688bef99a85a29aa
Pauline Chane <pauline.chane@gmail.com>
2020-09-22 01:12:24 -0700

    added add_planet method to solar_system.rb

--- lib/solar_system.rb
+++ lib/solar_system.rb
@@ -5,10 +5,19 @@
 
 # Solar System class object
 class SolarSystem
+  # field accessors
   attr_reader :star_name, :planets
-
+  # constructor
   def initialize(star_name)
     @star_name = star_name
     @planets = []
+  end
+
+  # other accessors
+
+  # mutators
+  # add planet to @planets array
+  def add_planet(planet)
+    planets.append(planet)
   end
 end
