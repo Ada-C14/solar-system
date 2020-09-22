05979a236c6b5a0d3b1c61040358bfc3c9ed5996
Lina Do <l.do5147@gmail.com>
2020-09-22 16:51:55 -0700

    refactor ArgumentErrors

--- lib/planet.rb
+++ lib/planet.rb
@@ -8,17 +8,12 @@ class Planet
     @distance_from_sun_km = distance_from_sun_km
     @fun_fact = fun_fact
 
-    if mass_kg <= 0
-      raise ArgumentError, 'Invalid mass'
-    end
+    raise ArgumentError, 'Invalid mass' if mass_kg <= 0
 
-    if distance_from_sun_km <= 0
-      raise ArgumentError, 'Invalid distance'
-    end
+    raise ArgumentError, 'Invalid distance' if distance_from_sun_km <= 0
   end
 
   def summary
     return "#{@name} is a #{@color} planet with a mass of #{@mass_kg}kg and a distance  \nof #{@distance_from_sun_km}km from the sun. #{@fun_fact}"
-    # return "Planet Name: #{@name}\nColor: #{@color}\nMass: #{@mass_kg} kg\nDistance from the Sun: #{@distance_from_sun_km} km\nFun fact: #{@fun_fact}\n\n"
   end
 end
