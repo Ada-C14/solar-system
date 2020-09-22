5e04554ebd27e541a2c18f0a5717b83571f6f60a
Pauline Chane <pauline.chane@gmail.com>
2020-09-22 10:22:54 -0700

    added list_planets method to solar_system.rb

--- lib/solar_system.rb
+++ lib/solar_system.rb
@@ -14,10 +14,20 @@ class SolarSystem
   end
 
   # other accessors
-
+  # list all planets in @planets array, return as string
+  def list_planets
+    planet_list = "Planets orbiting #{@star_name}"
+    (0...@planets.length).each do |i|
+      planet_list += "\n#{i+1}. #{@planets[i].name}"
+    end
+    return planet_list
+  end
+  
   # mutators
   # add planet to @planets array
   def add_planet(planet)
     planets.append(planet)
   end
+
+
 end
