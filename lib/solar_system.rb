616964e7b029e7a9b34a7918572bfc96d0cd0cfb
Marj E <marj@marjorieetienne.com>
2020-09-23 05:40:20 -0700

    added list planets method to SolarSystem class, added driver code in main

--- lib/solar_system.rb
+++ lib/solar_system.rb
@@ -1,12 +1,30 @@
-class SolarSytem
+class SolarSystem
   attr_reader :star_name, :planets
 
   def initialize(star_name)
     @star_name = star_name
-    @planets = []
+    @planets = %w[ Mercury Venus Earth Mars Jupiter]
   end
 
   def add_planet(planet_instance)
     @planets << planet_instance
+  end
+
+  #     1. Create a method `SolarSystem#list_planets`, which will _return_ (not `puts`) a string containing a list of all the planets in the system. The string should be formatted in this style:
+  #
+  #                                                                                                                                                                                             ```bash
+  #   Planets orbiting <star name>
+  #   1.  Mercury
+  #   2.  Venus
+  #   3.  Earth
+  #   4.  Mars
+  #   5.  Jupiter
+  #   ```
+
+  def list_planets
+    #can't get index to print for some reason that I am unaware of in either solution I've tried
+    # index = 0
+    # return "Planets orbiting the #{star_name}", @planets.each { |planet| "\n #{index += 1}. #{planet}" }
+    return "Planets orbiting the #{star_name}", @planets.each_with_index { |planet, index| "\n #{index + 1}. #{planet}" }
   end
 end
--- main.rb
+++ main.rb
@@ -1,13 +1,22 @@
 require_relative 'lib/planet'
+require_relative 'lib/solar_system'
 
 def main
-  earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
-  puts earth.fun_fact
+  # earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
+  # puts earth.fun_fact
 
   pluto = Planet.new('Pluto', 'unknown', 5.972e24, 1.496e8, 'Unknown')
   puts pluto.name
 
-  puts earth.summary
+  # puts earth.summary
+
+  solar_system = SolarSystem.new('Sol')
+
+  earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
+  solar_system.add_planet(earth)
+
+  list = solar_system.list_planets
+  puts list
 end
 
 main
