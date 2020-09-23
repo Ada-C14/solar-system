918613b394a75ab2be0057cdbe5f7dbcf5c96cb0
Jessica Chan <jwinchan@gmail.com>
2020-09-23 04:23:47 -0700

    fixed list planets; all options work, but no input validation

--- main.rb
+++ main.rb
@@ -1,21 +1,12 @@
 require_relative 'planet'
 require_relative 'solar_system'
 
-# def create_solar_system
-#   solar_system = SolarSystem.new('Zeus')
-#   tatooine = Planet.new('Tatooine', 'orange-ish', 6.34e48, 1.29e13, 'has two moons')
-#   pluto = Planet.new('Pluto', 'purple', 10, 2.8e293, 'counts as a planet in some books')
-#   solar_system.add_planet(tatooine)
-#   solar_system.add_planet(pluto)
-# end
-
 def main
   solar_system = SolarSystem.new('Zeus')
   tatooine = Planet.new('Tatooine', 'orange-ish', 6.34e48, 1.29e13, 'has two moons')
   pluto = Planet.new('Pluto', 'purple', 10, 2.8e293, 'counts as a planet in some books')
   solar_system.add_planet(tatooine)
   solar_system.add_planet(pluto)
-  list = solar_system.list_planets
 
   option = ""
 
@@ -30,12 +21,11 @@ def main
 
     case option
     when 'list planets'
-      puts list
+      puts solar_system.list_planets
     when 'get planet details'
       puts "Which planet's details do you want to know? "
       get_planet = gets.chomp
       puts solar_system.find_planet_by_name(get_planet).summary
-
     when 'add planet'
       puts 'Please enter new planet details'
       solar_system.add_planet(Planet.new(gets.chomp, gets.chomp, gets.chomp.to_f, gets.chomp.to_f, gets.chomp))
