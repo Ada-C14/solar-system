c0b3a8fabb7efe9098884747d8d3f5c4da397621
Kayla Johnson <kayla.elizabeth89@gmail.com>
2020-09-22 21:24:14 -0700

    Added an instance of found_planet to the main rb

--- main.rb
+++ main.rb
@@ -1,13 +1,21 @@
 require_relative 'planet'
-require 'colorize'
-require 'colorized_string'
-
+require_relative 'solar_system'
 
 def main
 
 end
 
-earth = Planet.new("earth", "blue-green",-1,2,"I live on it")
+solar_system = SolarSystem.new("Sol")
+earth = Planet.new("earth", "blue-green",1,2,"I live on it")
+solar_system.add_planet(earth)
 
-puts earth.summary
+# list = solar_system.list_planets
+# puts list
+# main
+
+found_planet = solar_system.find_planet_by_name("earth")
+puts found_planet
+
+puts found_planet.summary
+
 main
