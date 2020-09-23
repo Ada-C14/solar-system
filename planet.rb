0edd1e42845e05b4807e586ec50822d3f5813735
Kayla Johnson <kayla.elizabeth89@gmail.com>
2020-09-23 00:23:34 -0700

    Added 'return' to summary method

--- planet.rb
+++ planet.rb
@@ -2,16 +2,6 @@ class Planet
 
   attr_reader :name, :color, :mass_kg, :distance_from_sun_km, :fun_fact
 
-  # def mass_kg=(mass_kg)
-  #     raise ArgumentError, 'Mass must be greater than 0.' if mass_kg < 0
-  # end
-  #
-  # def distance_from_sun_km=(distance_from_sun_km)
-  #   if distance_from_sun_km < 0
-  #     raise ArgumentError.new('Distance from sun must be greater than 0.')
-  #   end
-  # end
-
   def initialize(name, color, mass_kg, distance_from_sun_km, fun_fact)
     @name = name
     @color = color
@@ -23,13 +13,9 @@ class Planet
   end
 
   def summary
-    " Name: #{@name}\n Color: #{@color}\n Mass: #{@mass_kg}\n Distance from the Sun: #{@distance_from_sun_km}\n Fun Fact: #{@fun_fact}"
+    summary = " Name: #{@name}\n Color: #{@color}\n Mass: #{@mass_kg}\n Distance from the Sun:#{@distance_from_sun_km}\n Fun Fact: #{@fun_fact}"
+    return summary
   end
-
-
-
-
-
 
 end
 
