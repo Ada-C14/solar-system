a808e92b0e82d9f3c8c2fe75e1543d7bb04188de
Kayla Johnson <kayla.elizabeth89@gmail.com>
2020-09-22 21:25:15 -0700

    Had to rework @mass_kg and @distance from sun--added the validation directly into the constuctor function

--- planet.rb
+++ planet.rb
@@ -2,26 +2,28 @@ class Planet
 
   attr_reader :name, :color, :mass_kg, :distance_from_sun_km, :fun_fact
 
-  def mass_kg=(mass_kg)
-      raise ArgumentError, 'Mass must be greater than 0.' if mass_kg < 0
-  end
-
-  def distance_from_sun_km=(distance_from_sun_km)
-    if distance_from_sun_km < 0
-      raise ArgumentError.new('Distance from sun must be greater than 0.')
-    end
-  end
+  # def mass_kg=(mass_kg)
+  #     raise ArgumentError, 'Mass must be greater than 0.' if mass_kg < 0
+  # end
+  #
+  # def distance_from_sun_km=(distance_from_sun_km)
+  #   if distance_from_sun_km < 0
+  #     raise ArgumentError.new('Distance from sun must be greater than 0.')
+  #   end
+  # end
 
   def initialize(name, color, mass_kg, distance_from_sun_km, fun_fact)
     @name = name
     @color = color
-    self.mass_kg = mass_kg
-    self.distance_from_sun_km = distance_from_sun_km
+    @mass_kg = mass_kg
+    raise ArgumentError, 'Mass must be greater than 0.' if mass_kg < 0
+    @distance_from_sun_km = distance_from_sun_km
+    raise ArgumentError, 'Distance from sun must be greater than 0.' if distance_from_sun_km < 0
     @fun_fact = fun_fact
   end
 
   def summary
-    " Name: #{@name}\n Color: #{@color}\n Mass: #{mass_kg}\n Distance from the Sun: #{distance_from_sun_km}\n Fun Fact: #{@fun_fact}"
+    " Name: #{@name}\n Color: #{@color}\n Mass: #{@mass_kg}\n Distance from the Sun: #{@distance_from_sun_km}\n Fun Fact: #{@fun_fact}"
   end
 
 
