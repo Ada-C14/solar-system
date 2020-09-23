8d49a9dc0052a2e451f380070e5d16c9448322d2
Kayla Johnson <kayla.elizabeth89@gmail.com>
2020-09-23 00:22:48 -0700

    found bug in 'view planet details' option...but now gone

--- main.rb
+++ main.rb
@@ -3,19 +3,76 @@ require_relative 'solar_system'
 
 def main
 
+  #Create a solar system, planets, and add them to the planets array in the solar system
+  solar_system = SolarSystem.new("luna")
+
+  koopa_troopa_town = Planet.new("koopa troopa town","red and green", 6.972e24, 1.283e7, "It takes a while for inhabitants to come out of their shell.")
+  solar_system.add_planet(koopa_troopa_town)
+
+  yoshi_land = Planet.new("yoshi land", "green", 2, 1.5, "My full planet name is T. Yoshisaur Munchakoopas" )
+  solar_system.add_planet(yoshi_land)
+
+  mario_world = Planet.new("mario world", "red", 5.432e56, 100000, "You'll find that everything here is super!")
+  solar_system.add_planet(mario_world)
+
+  earth = Planet.new("earth", "blue-green",6.972e24,1.283e7,"I live on it")
+  solar_system.add_planet(earth)
+
+  bowserville = Planet.new("bowserville", "orange, red, and green", 100000000e34, 60000, "Bwa ha ha ha!" )
+  solar_system.add_planet(bowserville)
+
+  luigi_lagoon = Planet.new("luigi lagoon", "green", 343439, 232939, "Luigi TIme!!")
+  solar_system.add_planet(luigi_lagoon)
+
+  princess_peach_paradise = Planet.new("princess peach paradise", "peach", 34342, 45645, "Peachy!")
+  solar_system.add_planet(princess_peach_paradise)
+
+
+
+  #Control loop that repeatedly asks user what to do next
+  repeat = true
+
+  until repeat == false
+    puts "*" * 50
+    puts "Marioooo time! What would you like to do next?\nYou can:\n"
+    puts "1. See a list of planets by typing 'list'\n"
+    puts "2. View planet details of your favorite planet by typing 'details'\n"
+    puts "3. Exit the program by typing 'exit'\n\n"
+    puts "Waiting for your input.... "
+    input = gets.chomp
+
+    case
+    when input == "list"
+        repeat = false
+        list = solar_system.list_planets
+        puts list
+    when input == "details"
+      puts "What planet are you interested in learning more about?"
+      input = gets.chomp.to_s
+      summary = solar_system.find_planet_by_name(input)
+      puts summary
+      repeat = false
+    end
+  end
 end
 
-solar_system = SolarSystem.new("Sol")
-earth = Planet.new("earth", "blue-green",1,2,"I live on it")
-solar_system.add_planet(earth)
-
-# list = solar_system.list_planets
-# puts list
-# main
-
-found_planet = solar_system.find_planet_by_name("earth")
-puts found_planet
-
-puts found_planet.summary
-
 main
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# main
+#
+
+# end
+# main
