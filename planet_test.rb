73e8e5c1cedba6fd5b59cc8d6b4c0ba8a0ea6327
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-09-22 22:44:59 -0700

    add Planet summary test

--- planet_test.rb
+++ planet_test.rb
@@ -16,4 +16,11 @@ describe "planet" do
 
     expect(cybertron).must_be_instance_of Planet
   end
+
+  it "will return a summary of our Planet instance" do
+    mercury = Planet.new('Mercury', 'burnt orange', 330, 35.98, 'Aliens live here!')
+    summary = mercury.summary
+
+    assert_match /Mercury is 35\.98 km away from the Sun\./, summary
+  end
 end
