0405cd89b92326e37fa7135d97144283b708bac8
marks214 <aimee.oz.x@gmail.com>
2020-09-22 19:11:30 -0700

    finished Wave 2

--- planet.rb
+++ planet.rb
@@ -11,7 +11,7 @@ class Planet
   end
 
   def summary
-    planet_info = "#{@name} is a #{@color} planet, approximately #{@distance_from_sun_km}" +
+    planet_info = "#{@name} is a #{@color} planet, approximately #{@distance_from_sun_km}" \
       "km from the sun, with a mass of about #{@mass_kg} kg. \n#{@fun_fact}"
     return planet_info
   end
