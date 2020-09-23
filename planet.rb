5f01cc7af52ed9364edc88e4df338166de671ee7
Kalkidan Ashenafi <kalkidanashenafi@gmail.com>
2020-09-22 22:30:09 -0700

    Wave 1 no optionals

---
+++ planet.rb
@@ -0,0 +1,16 @@
+class Planet
+  attr_reader :name, :color, :mass_kg, :distance_from_sun, :fun_fact
+
+  def initialize (name, color, mass_kg, distance_from_sun, fun_fact)
+    @name = name
+    @color = color
+    @mass_kg = mass_kg
+    @distance_from_sun = distance_from_sun
+    @fun_fact = fun_fact
+  end
+
+  def summary
+    return "Planet #{@name} is the color #{@color}. It's mass is #{@mass_kg} kg and it's distance from the sun is #{@distance_from_sun}. Fun fact => #{@fun_fact}!"
+  end
+
+end
