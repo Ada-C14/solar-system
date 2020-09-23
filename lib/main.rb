7db5f3d6d139ef4d9200c448305990a30de13bad
Beatrice T <be@trice.codes>
2020-09-22 19:10:45 -0700

    Restructured parameter type of menu() function to fix bug

--- lib/main.rb
+++ lib/main.rb
@@ -8,14 +8,18 @@ def menu(choices)
   choices.each_with_index { |choice, i| puts "#{i+1}. #{choice}"}
 end
 
-def get_choice(max_choice)
+def get_choice(choices)
   choice = gets.to_i
-  until choice > 0 && choice <= max_choice
+  until choice > 0 && choice <= choices.length
     puts "Incorrect choice entered"
-    menu
+    menu(choices)
     choice = gets.to_i
   end
   return choice
+end
+
+def get_planet_details
+
 end
 
 def main
@@ -24,20 +28,25 @@ def main
                        favorite_animal: "Meerkat", pronoun: "Her")
   pluto = Planet.new('Pluto', 'Red', 1, 9, "Forever angry at it's planetary exclusion")
   meow = Planet.new('Meow', 'Yellow', 33, 39, 'Has two ears and one tail', pronoun: 'Fur')
+  solar.add_planet(jupiter)
+  solar.add_planet(pluto)
+  solar.add_planet(meow)
 
   puts Rainbow("Welcome to the #{solar.star_name} system").color(:yellow)
-  choices = ['List Planets', 'Exit']
+  choices = ['List Planets', 'Planet Details', 'Exit']
   menu(choices)
-  choice = get_choice(choices.length)
+  choice = get_choice(choices)
   until choice == choices.length
     case choice
     when 1
       puts solar.list_planets
+    when 2
+      get_planet_details
     end
 
     puts "What else?"
     menu(choices)
-    choice = get_choice(2)
+    choice = get_choice(choices)
   end
 
 
