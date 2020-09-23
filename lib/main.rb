80f74ca95e326f740f222ae9e646fa05e1f946a4
Pauline Chane <pauline.chane@gmail.com>
2020-09-23 04:30:30 -0700

    Minor DRY edits, added ability for user to obtain distance between two planets already in solar system.

--- lib/main.rb
+++ lib/main.rb
@@ -10,7 +10,7 @@ require_relative 'solar_system'
 # defs for control loop actions that take a little more time
 # get detail for planet and account for invalid input
 def get_planet_detail(solar_system_obj)
-  print "Enter name of Planet you wish to learn more about: "
+  print "Enter name of Planet: "
   input = gets.chomp
 
   # don't exit until name of findable planet is entered
@@ -19,8 +19,7 @@ def get_planet_detail(solar_system_obj)
     input = gets.chomp
   end
 
-  # for CLI purposes, here is a puts statement, usually better to return a string and print it though
-  puts solar_system_obj.find_planet_by_name(input).summary
+  return solar_system_obj.find_planet_by_name(input)
 end
 
 # get and validate user input for adding planet with new info
@@ -58,7 +57,21 @@ def user_add_planet(solar_system_obj)
   new_planet = Planet.new(name, color, mass, distance_from_sun, fun_fact)
 
   solar_system_obj.add_planet(new_planet)
+  return true
 end
+
+# get user input for two planets CURRENTLY in the solar system and return the distance between them
+# returns a 3 element array that allows for printing a string stating the distance between two planets.
+def user_input_distance(solar_system_obj)
+  puts "ENTER FIRST PLANET"
+  planet1 = get_planet_detail(solar_system_obj)
+
+  puts "ENTER SECOND PLANET"
+  planet2 = get_planet_detail(solar_system_obj)
+
+  return [planet1.name, planet2.name, solar_system_obj.distance_between(planet1.name, planet2.name)] # name of planets and distance between them
+end
+
 
 # code for driver itself
 def main
@@ -80,35 +93,43 @@ def main
   puts '|1. LIST PLANETS CURRENTLY IN SOLAR SYSTEM                              |'
   puts '|2. OBTAIN DETAILS ON A PLANET CURRENTLY IN SOLAR SYSTEM                |'
   puts '|3. ADD A PLANET TO THE SOLAR SYSTEM                                    |'
-  puts '|9. EXIT                                                                |'
+  puts '|4. FIND THE DISTANCE BETWEEN TWO PLANETS (IF LINED UP IN STRAIGHT LINE)|'
+  puts '|5. EXIT                                                                |'
+  puts '+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+'
   print "Enter your selection: "
   user_input = gets.chomp.to_i
 
-  while user_input != 9 do
+  while user_input != 5 do
     case user_input
-    when 1
+    when 1 # list planets
       puts "---------------------------"
       puts "LISTING PLANETS ..."
       puts solar_system.list_planets
       puts "---------------------------"
-      print "ACTION COMPLETE. YOU CAN NOW:\nEnter 1 to LIST PLANETS again\nEnter 2 to GET PLANET DETAILS\nEnter 3 to ADD A PLANET\nEnter 9 to EXIT: "
-    when 2
+    when 2 # planet details
       puts "---------------------------"
       puts "GETTING PLANET DETAILS ..."
-      get_planet_detail(solar_system)
+      planet_det = get_planet_detail(solar_system)
+      puts planet_det.summary
       puts "---------------------------"
-      print "ACTION COMPLETE. YOU CAN NOW:\nEnter 1 to LIST PLANETS\nEnter 2 to GET PLANET DETAILS again\nEnter 3 to ADD A PLANET\nEnter 9 to EXIT: "
-    when 3
+    when 3 # add planet
       puts "---------------------------"
       puts "ADDING PLANET ..."
       user_add_planet(solar_system)
       puts "---------------------------"
-      print "ACTION COMPLETE. YOU CAN NOW:\nEnter 1 to LIST PLANETS\nEnter 2 to GET PLANET DETAILS\nEnter 3 to ADD ANOTHER PLANET\nEnter 9 to EXIT: "
-    when 9
+    when 4 # distance between two planets
+      puts "---------------------------"
+      puts "GETTING DISTANCE BETWEEN TWO PLANETS ..."
+      distance_info = user_input_distance(solar_system)
+      puts "Assuming they are lined up in a straight line, the distance between %s and %s is %.2f" % distance_info
+      puts "---------------------------"
+    when 5 # exit case
       break
-    else
-      print "INVALID INPUT. YOU CAN:\nEnter 1 to LIST PLANETS\nEnter 2 to GET PLANET DETAILS\nEnter 3 to ADD A PLANET\nEnter 9 to EXIT: "
+    else # invalid input
+      print "INVALID INPUT. "
     end
+    puts "CURRENTLY YOU CAN:\nEnter 1 to LIST PLANETS\nEnter 2 to GET PLANET DETAILS\nEnter 3 to ADD A PLANET\nEnter 4 to FIND THE DISTANCE BETWEEN TWO PLANETS\nEnter 5 to EXIT"
+    print "Select an option: "
     user_input = gets.chomp.to_i
   end
 
