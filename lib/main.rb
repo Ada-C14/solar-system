e261c55d07aa1af39934519638628012b48034b3
Pauline Chane <pauline.chane@gmail.com>
2020-09-23 03:45:32 -0700

    Added ability for user to add planet to solar system, incl. some error handling

--- lib/main.rb
+++ lib/main.rb
@@ -1,6 +1,6 @@
 # Pauline Chane (@PaulineChane)
 # Ada Developers Academy C14
-# Solar System Assignment - main.rb Main Class
+# Solar System Assignment - main.rb Main Class Driver/CLI methods
 # 09/23/2020
 
 # main.rb
@@ -8,6 +8,7 @@ require_relative 'planet'
 require_relative 'solar_system'
 
 # defs for control loop actions that take a little more time
+# get detail for planet and account for invalid input
 def get_planet_detail(solar_system_obj)
   print "Enter name of Planet you wish to learn more about: "
   input = gets.chomp
@@ -22,6 +23,44 @@ def get_planet_detail(solar_system_obj)
   puts solar_system_obj.find_planet_by_name(input).summary
 end
 
+# get and validate user input for adding planet with new info
+def user_add_planet(solar_system_obj)
+  # prompt/input for all input parameters stored in individual variables
+  print "Enter planet name: "
+  name = gets.chomp
+
+  print "Enter planet color: "
+  color = gets.chomp
+
+  # mass must be greater than 0
+  print "Enter planet mass (in kg): "
+  mass = gets.chomp.to_f
+
+  while mass <= 0 do
+    puts "Invalid mass (in kg) input - must be greater than zero"
+    print "Enter planet mass (in kg): "
+    mass = gets.chomp.to_f
+  end
+
+  # distance from sun must be greater than zero
+  print "Enter planet's distance from the sun (in km): "
+  distance_from_sun = gets.chomp.to_f
+
+  while distance_from_sun <= 0 do
+    puts "Invalid distance from sun (in km) input - must be greater than zero"
+    print "Enter planet's distance from the sun (in km): "
+    distance_from_sun = gets.chomp.to_f
+  end
+
+  print "Enter planet fun fact: "
+  fun_fact = gets.chomp
+
+  new_planet = Planet.new(name, color, mass, distance_from_sun, fun_fact)
+
+  solar_system_obj.add_planet(new_planet)
+end
+
+# code for driver itself
 def main
   # create base solar_system with some planets
   solar_system = SolarSystem.new('Sol')
@@ -37,9 +76,10 @@ def main
   puts 'WELCOME TO CLISSIN (Command Line Interface Solar System Info Navigation)!'
   puts '+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+'
   puts '|                              MENU SELECT                              |'
-  puts '|_____Please enter the number corresponding to your desired action:_____|'
+  puts '|_____Please enter the NUMBER corresponding to your desired action:_____|'
   puts '|1. LIST PLANETS CURRENTLY IN SOLAR SYSTEM                              |'
   puts '|2. OBTAIN DETAILS ON A PLANET CURRENTLY IN SOLAR SYSTEM                |'
+  puts '|3. ADD A PLANET TO THE SOLAR SYSTEM                                    |'
   puts '|9. EXIT                                                                |'
   print "Enter your selection: "
   user_input = gets.chomp.to_i
@@ -47,22 +87,32 @@ def main
   while user_input != 9 do
     case user_input
     when 1
+      puts "---------------------------"
       puts "LISTING PLANETS ..."
       puts solar_system.list_planets
-      print "Action complete. Enter 1 to LIST PLANETS again, 2 to GET PLANET DETAILS, or 9 to EXIT: "
+      puts "---------------------------"
+      print "ACTION COMPLETE. YOU CAN NOW:\nEnter 1 to LIST PLANETS again\nEnter 2 to GET PLANET DETAILS\nEnter 3 to ADD A PLANET\nEnter 9 to EXIT: "
     when 2
+      puts "---------------------------"
       puts "GETTING PLANET DETAILS ..."
       get_planet_detail(solar_system)
-      puts "Action complete. Enter 1 to LIST PLANETS, 2 to GET PLANET DETAILS again, or 9 to EXIT: "
+      puts "---------------------------"
+      print "ACTION COMPLETE. YOU CAN NOW:\nEnter 1 to LIST PLANETS\nEnter 2 to GET PLANET DETAILS again\nEnter 3 to ADD A PLANET\nEnter 9 to EXIT: "
+    when 3
+      puts "---------------------------"
+      puts "ADDING PLANET ..."
+      user_add_planet(solar_system)
+      puts "---------------------------"
+      print "ACTION COMPLETE. YOU CAN NOW:\nEnter 1 to LIST PLANETS\nEnter 2 to GET PLANET DETAILS\nEnter 3 to ADD ANOTHER PLANET\nEnter 9 to EXIT: "
     when 9
       break
     else
-      print "Invalid input. Enter 1 to LIST PLANETS, 2 to GET PLANET DETAILS, or 9 to EXIT: "
+      print "INVALID INPUT. YOU CAN:\nEnter 1 to LIST PLANETS\nEnter 2 to GET PLANET DETAILS\nEnter 3 to ADD A PLANET\nEnter 9 to EXIT: "
     end
     user_input = gets.chomp.to_i
   end
 
-  puts "Thank you for using the CLISSIN. See you Space Cowpoke!"
+  puts "Thank you for using CLISSIN. See you Space Cowpoke!"
 
 end
 
