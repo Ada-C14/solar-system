86758a73880d52010d716ea7d57d9fba04de0a85
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2020-09-23 11:51:30 -0700

    rethought some of those Argument Errors

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 require_relative 'planet'
 require_relative 'solar_system'
 
-def main
+def birth_of_solar_system
   solar_system = SolarSystem.new('Solar System')
   earth = Planet.new('Earth', 'blue-green', 5.972e24, 150.11, 'is the only planet known to support life')
   venus = Planet.new('Venus', 'grey', 4.868e24, 107.85, 'spins in the opposite direction to most planets')
@@ -11,10 +11,15 @@ def main
   solar_system.add_planet(venus)
   solar_system.add_planet(neptune)
 
+  return solar_system
+end
+
+def main
+  solar_system = birth_of_solar_system
   puts "Welcome to Anya's Solar System assignment!"
   while true
     puts "\n**********************************************"
-    print "\nWhat would you like to do next? (list planets, planet details, add planet, or exit): "
+    print "\nWhat would you like to do next? (options: list planets, planet details, add planet, or exit): "
     user_input = gets.chomp
     case user_input
     when "list planets"
@@ -29,8 +34,8 @@ def main
     when "exit"
       return false
     else
-      # same as in solar_system, would like to trade this in for raise and rescue but having trouble figuring out how to make it work.
-      raise ArgumentError.new("Didn't choose valid option (entered #{user_input})")
+      # raise ArgumentError.new("Didn't choose valid option (entered #{user_input})")
+      puts "Didn't choose valid option (entered: #{user_input})"
     end
   end
 end
