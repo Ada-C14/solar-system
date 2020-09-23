1496c9343b39ded2e57c87fe2bea6ee41a8b46e1
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-09-22 21:12:27 -0700

    address a number in string data type

--- test/planet_test.rb
+++ test/planet_test.rb
@@ -16,6 +16,14 @@ describe 'tests for planet class' do
     # Assert 
     expect(tiny_planet.mass_kg).must_equal 1
     expect(tiny_planet.distance_from_sun_km).must_equal 10000
+
+    tiny_planet2 = Planet.new('Tiny planet', 'pale pink', "4", 10000, 'Pale pink color is becuase of cherry blossom')
+    expect(tiny_planet2.mass_kg.to_f).must_equal 4.0
+
+    tiny_planet3 = Planet.new('Tiny planet', 'pale pink', 1, "40000", 'Pale pink color is becuase of cherry blossom')
+    expect(tiny_planet3.distance_from_sun_km.to_f).must_equal 40000.0
+
+
   end
 
   it 'raises an ArgumentError for mass_kg is not a number greater than 0' do
