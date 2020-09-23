cad9a1562f9ade5af15e01e317afa773b1079607
Marj E <marj@marjorieetienne.com>
2020-09-22 22:49:54 -0700

    added lib directory, added initialize parameters

--- lib/planet.rb
+++ lib/planet.rb
@@ -1,7 +1,7 @@
 class Planet
   attr_reader :name, :color, :mass_kg, :distance_from_sun_km, :fun_fact
 
-  def initialize
+  def initialize(name, color, mass_kg, distance_from_sun_km, fun_fact)
     @name = name
     @color = color
     @mass_kg = mass_kg
