54ea367e67d97dee45be403737ae7611e90ffc8e
Stacy <lundquist.stacy@gmail.com>
2020-09-22 19:06:51 -0700

    Ughhhhh Wave 2 Step 5

--- main.rb
+++ main.rb
@@ -10,6 +10,14 @@ def main
 
   list = solar_system.list_planets
   puts list
+
+  puts earth.summary
+
+  found_planet = solar_system.find_planet_by_name('Earth')
+  puts found_planet
+  
+  puts found_planet.summary
+
 end
 
 main
--- solar_system.rb
+++ solar_system.rb
@@ -16,13 +16,19 @@ class SolarSystem
     planets = []
     list = 1
     @planets.each do |planet|
-      planets << ["#{list}, #{planet.name}"]
+      planets << ["#{list}. #{planet.name}"]
       list += 1
     end
 
     list_planets = Terminal::Table.new :title => "Planets orbiting #{star_name}", :rows => planets
 
     return list_planets
+  end
+
+  def find_planet_by_name(name)
+    planet_name = name.downcase
+    found_planet = @planets.find {|planet| planet.name.downcase == planet_name}
+    return found_planet
   end
 
 end
