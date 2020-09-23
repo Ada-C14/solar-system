f00913f8e250cac513e8086dc7d69219df495ff0
Anna Kim <anna.kim93@gmail.com>
2020-09-23 10:47:02 -0400

    reversed previous decision and included planet.name in list_planets function

--- lib/solar_system.rb
+++ lib/solar_system.rb
@@ -8,12 +8,17 @@ class SolarSystem
   end
 
   def add_planet(planet)
-    @planets << planet.name
+    @planets << planet
   end
 
   def list_planets
     summary_list = "Planets orbiting <#{@star_name}>\n"
-    @planets.each.with_index(1) { |planet, idx| summary_list << "#{idx}. #{planet}\n"}
+    @planets.each.with_index(1) { |planet, idx| summary_list << "#{idx}. #{planet.name}\n"}
     return summary_list
+  end
+
+  def find_planet_by_name(planet)
+    planet.capitalize!
+    return
   end
 end
