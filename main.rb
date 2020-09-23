3fc0cdbe651b8750a6f52f5d79b9f4ae619bcbff
Ringo Alcock <ringo.alcock@gmail.com>
2020-09-22 20:14:09 -0700

    add colorize and clean up comments

--- main.rb
+++ main.rb
@@ -1,83 +1,70 @@
 require_relative "planet"
 require_relative "solar_system"
+require "colorize"
 
 def main
-  solar_system = SolarSystem.new("Sol")
+  solar_system = SolarSystem.new("Trek Star")
 
-  trill = Planet.new("Trill", "Purple", 1.372e24, 1.206e5, "It is full of caves!")
+  trill = Planet.new("Trill", "purple", 1.372e24, 1.206e5, "It is full of caves")
   solar_system.add_planet(trill)
 
-  cardassia = Planet.new("Cardassia", "Brown", 5.012e16, 2.365e7, "It is very hot!")
+  cardassia = Planet.new("Cardassia", "brown", 5.012e16, 2.365e7, "It is very hot")
   solar_system.add_planet(cardassia)
 
-  earth = Planet.new("Qo'noS", "Green", 5.972e24, 1.496e8, "It is hard to spell!")
-  solar_system.add_planet(earth)
+  qonos = Planet.new("Qo'noS", "green", 5.972e24, 1.496e8, "It is hard to spell")
+  solar_system.add_planet(qonos)
 
-  bajor = Planet.new("Bajor", "Blue", 3.721e4, 1.916e12, "It is by a wormhole!")
+  bajor = Planet.new("Bajor", "blue", 3.721e4, 1.916e12, "It is by a wormhole")
   solar_system.add_planet(bajor)
 
-  selection = ""
+  def prompt_user
+    puts "What would you like to do?".colorize(:blue)
+    puts "list planets\nplanet details\nadd planet\nfind distance\nexit".colorize(:cyan)
+    return gets.chomp.downcase
+  end
+
+  selection = prompt_user
   while selection != "exit"
-    puts "What would you like to do?\nlist planets\nplanet details\nadd planet\nfind distance\nexit"
-    selection = gets.chomp.downcase
     case selection
+
     when "list planets"
       puts solar_system.list_planets
+
     when "planet details"
-      # add a helper method to check that planet is actually in solar_system?
-      puts "What planet would you like to know about?\n#{solar_system.list_planets}"
+      puts "What planet would you like to know about?".colorize(:blue)
+      puts "#{solar_system.list_planets}".colorize(:cyan)
       curious_planet = gets.chomp
       curious_planet_answers = solar_system.find_planet_by_name(curious_planet)
-      puts curious_planet_answers.summary
+      # in case find_planet_by_name returns nil
+      puts curious_planet_answers.summary if curious_planet_answers
+
     when "add planet"
-      puts "What is the name of this new planet?"
+      puts "What is the name of this new planet?".colorize(:blue)
       new_name = gets.chomp
-      puts "What color is the new planet?"
+      puts "What color is the new planet?".colorize(:blue)
       new_color = gets.chomp
-      puts "What is the mass of the new planet in kg?"
+      puts "What is the mass of the new planet in kg?".colorize(:blue)
       new_mass = gets.chomp.to_f
-      puts "How far is the new planet from #{solar_system.star_name} in km?"
+      puts "How far is the new planet from #{solar_system.star_name} in km?".colorize(:blue)
       new_distance = gets.chomp.to_f
-      puts "What is a fun fact about the new planet?"
+      puts "What is a fun fact about the new planet?".colorize(:blue)
       new_fact = gets.chomp
       new_planet = Planet.new(new_name, new_color, new_mass, new_distance, new_fact)
       solar_system.add_planet(new_planet)
+
     when "find distance"
-      # add a helper method to check that planet is actually in solar_system?
-      puts "what is the planet you are starting from?"
+      puts "what is the planet you are starting from?".colorize(:blue)
       planet1 = gets.chomp
-      puts "what is the planet you are ending on?"
+      puts "what is the planet you are ending on?".colorize(:blue)
       planet2 = gets.chomp
       puts solar_system.distance_between(planet1, planet2)
+
     else
-      puts "that is not a valid selection"
-      # TODO Ringo make this make sense
-      # if I do not have a
-      # selection = gets.chomp
-      # line, than putting "exit" puts "that is not a valid selection" before ending
-      # if I do have it then it bugs user to enter their selection AGAIn
-      # at the top of the loop
+      puts "that is not a valid selection".colorize(:blue)
     end
+
+    selection = prompt_user
   end
 end
 
-# TODO Ringo get rid of this
-# def scratchpad
-#   solar_system = SolarSystem.new("Sol")
-#
-#   trill = Planet.new("Trill", "Purple", 1.372e24, 1.206e5, "It is full of caves!")
-#   solar_system.add_planet(trill)
-#
-#   cardassia = Planet.new("Cardassia", "Brown", 5.012e16, 2.365e7, "It is very hot!")
-#   solar_system.add_planet(cardassia)
-#
-#   earth = Planet.new("Qo'noS", "Green", 5.972e24, 1.496e8, "It is hard to spell!")
-#   solar_system.add_planet(earth)
-#
-#   bajor = Planet.new("Bajor", "Blue", 3.721e4, 1.916e12, "It is by a wormhole!")
-#   solar_system.add_planet(bajor)
-#
-# end
-
 main
-
