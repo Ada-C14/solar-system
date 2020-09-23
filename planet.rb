81bc907827691184fa9801203ec8327cfce674fe
Kalkidan Ashenafi <kalkidanashenafi@gmail.com>
2020-09-23 01:38:37 -0700

    Wave 2 w/ 1 optional

--- planet.rb
+++ planet.rb
@@ -2,7 +2,7 @@ class Planet
   attr_reader :name, :color, :mass_kg, :distance_from_sun, :fun_fact
 
   def initialize (name, color, mass_kg, distance_from_sun, fun_fact)
-    @name = name
+    @name = name.capitalize
     @color = color
     @mass_kg = mass_kg
     @distance_from_sun = distance_from_sun
