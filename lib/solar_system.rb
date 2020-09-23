9befd9ecf0be6e912f2359ff172fe5e1a0cef920
Marj E <marj@marjorieetienne.com>
2020-09-23 07:37:03 -0700

    fixed list_planets method solution, added find_planet_by_name method to SolarSystem class

--- lib/solar_system.rb
+++ lib/solar_system.rb
@@ -3,33 +3,28 @@ class SolarSystem
 
   def initialize(star_name)
     @star_name = star_name
-    @planets = %w[ Mercury Venus Earth Mars Jupiter]
+    @planets = []
   end
 
   def add_planet(planet_instance)
     @planets << planet_instance
   end
 
-  #     1. Create a method `SolarSystem#list_planets`, which will _return_ (not `puts`) a string containing a list of all the planets in the system. The string should be formatted in this style:
-  #
-  #                                                                                                                                                                                             ```bash
-  #   Planets orbiting <star name>
-  #   1.  Mercury
-  #   2.  Venus
-  #   3.  Earth
-  #   4.  Mars
-  #   5.  Jupiter
-  #   ```
-
   def list_planets
-    #can't get index to print for some reason that I am unaware of in either solution I've tried
-    # index = 0
-    # return "Planets orbiting the #{star_name}", @planets.each { |planet| "\n #{index += 1}. #{planet}" }
-    return "Planets orbiting the #{star_name}", @planets.each_with_index { |planet, index| "\n #{index + 1}. #{planet}" }
+    list = "Planets orbiting the #{star_name}"
+    @planets.length.times do |index|
+      list_item = "\n #{index + 1}. #{@planets[index].name}"
+      list += list_item
+    end
+    return list
   end
 
   def find_planet_by_name(planet)
     planet = planet.downcase
-    return @planets.select { |planet_name| planet_name.downcase == planet }
+    planet_instance = nil
+    @planets.length.times do |index|
+      planet_instance = @planets[index] if @planets[index].name.downcase == planet
+    end
+    return planet_instance
   end
 end
--- main.rb
+++ main.rb
@@ -2,21 +2,24 @@ require_relative 'lib/planet'
 require_relative 'lib/solar_system'
 
 def main
-  # earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
-  # puts earth.fun_fact
-
+  puts "Wave 1"
+  earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
+  puts earth.fun_fact
   pluto = Planet.new('Pluto', 'unknown', 5.972e24, 1.496e8, 'Unknown')
   puts pluto.name
+  puts earth.summary
 
-  # puts earth.summary
-
+  puts "Wave 2"
   solar_system = SolarSystem.new('Sol')
-
-  earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
   solar_system.add_planet(earth)
+  solar_system.add_planet(pluto)
 
   list = solar_system.list_planets
   puts list
+
+  found_planet = solar_system.find_planet_by_name('Earth')
+  puts "This is the planet that was found: #{found_planet}"
+  puts found_planet.summary
 end
 
 main
