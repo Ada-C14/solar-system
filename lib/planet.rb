5977046346b4f08bf732e6fd291a64d82d740265
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-09-22 22:27:39 -0700

    revise the data dype for mass_by & distance_from_sun_km

--- lib/planet.rb
+++ lib/planet.rb
@@ -5,9 +5,11 @@ class Planet
     @mass_kg = mass_kg
     
     raise ArgumentError.new("Mass(kg) should be a number that greater than zero: #{ @mass_kg }") if !(@mass_kg.to_f > 0)
+    @mass_kg = mass_kg.to_f
     
     @distance_from_sun_km = distance_from_sun_km
     raise ArgumentError.new("Distance from sun (km) should be a number that greater than zero: #{ @distance_from_sun_km }") if !(@distance_from_sun_km.to_f > 0)
+    @distance_from_sun_km = distance_from_sun_km.to_f
 
     @name = name
     @color = color
